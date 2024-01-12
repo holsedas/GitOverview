ca4288d29b4c9f03ae5d972371bc062261af1896
Viktar Holub <holsedas.java@gmail.com>
2024-01-12 04:10:28 +0300

    Adding new class BaseEntity

---
+++ src/main/java/com/holsedas/gitoverview/BaseEntity.java
@@ -0,0 +1,25 @@
+package com.holsedas.gitoverview;
+
+public class BaseEntity {
+
+    private Long id;
+
+    public BaseEntity(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "BaseEntity{" +
+               "id=" + id +
+               '}';
+    }
+}
