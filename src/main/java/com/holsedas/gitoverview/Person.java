68eced15913090e761f4683c27541d2bbf6af0fb
Viktar Holub <holsedas.java@gmail.com>
2024-01-12 05:09:14 +0300

    Adding new class Person

---
+++ src/main/java/com/holsedas/gitoverview/Person.java
@@ -0,0 +1,38 @@
+package com.holsedas.gitoverview;
+
+public class Person extends BaseEntity {
+
+    private String firstName;
+    private String lastName;
+
+    public Person(Long id, String firstName, String lastName) {
+        super(id);
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+               "id='" + super.getId() + '\'' +
+               "firstName='" + firstName + '\'' +
+               ", lastName='" + lastName + '\'' +
+               '}';
+    }
+}
